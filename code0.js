fd4c2696d0206a1a273c6f09018b369b9efe442e
caymankun <111965236+caymankun@users.noreply.github.com>
2023-03-11 13:59:33 +0900

    Add files via upload

---
+++ code0.js
@@ -0,0 +1,47 @@
+gdjs.Untitled_32sceneCode = {};
+gdjs.Untitled_32sceneCode.GDNewTiledSpriteObjects1= [];
+gdjs.Untitled_32sceneCode.GDNewTiledSpriteObjects2= [];
+gdjs.Untitled_32sceneCode.GDFlatDarkJoystickObjects1= [];
+gdjs.Untitled_32sceneCode.GDFlatDarkJoystickObjects2= [];
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects1= [];
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects2= [];
+
+gdjs.Untitled_32sceneCode.conditionTrue_0 = {val:false};
+gdjs.Untitled_32sceneCode.condition0IsTrue_0 = {val:false};
+gdjs.Untitled_32sceneCode.condition1IsTrue_0 = {val:false};
+
+
+gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
+
+{
+
+
+gdjs.Untitled_32sceneCode.condition0IsTrue_0.val = false;
+{
+gdjs.Untitled_32sceneCode.condition0IsTrue_0.val = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+}if (gdjs.Untitled_32sceneCode.condition0IsTrue_0.val) {
+{gdjs.playerAuthentication.displayAuthenticationBanner(runtimeScene);
+}}
+
+}
+
+
+};
+
+gdjs.Untitled_32sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.Untitled_32sceneCode.GDNewTiledSpriteObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewTiledSpriteObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDFlatDarkJoystickObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDFlatDarkJoystickObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects2.length = 0;
+
+gdjs.Untitled_32sceneCode.eventsList0(runtimeScene);
+
+return;
+
+}
+
+gdjs['Untitled_32sceneCode'] = gdjs.Untitled_32sceneCode;
